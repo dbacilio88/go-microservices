cd0cc766af853f4d2743ae7b4080a0ddb561220d
cbaciliodev <dbacilio88@outlook.es>
2024-05-27 22:05:50 -0500

    go

--- src/fundamental/example/values.go
+++ src/fundamental/example/values.go
@@ -1,12 +1,17 @@
-package example
+package main
 
 import "fmt"
 import "math"
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
-	fmt.Print(futureValue)
+	const inflationRate = 2.5
+	var investmentAmount float64
+	expectedReturnRate := 5.5
+	var years = 10.0
+	fmt.Scan(&investmentAmount)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+
 }
